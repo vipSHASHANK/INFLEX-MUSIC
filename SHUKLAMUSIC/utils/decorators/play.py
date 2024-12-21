58332b2b2c7fc59a42cb9bf3e537fb6d6bda80eb
SHASHANK <primiumshukla474@gmail.com>
2024-12-21 11:06:18 +0530

    Update play.py

--- SHUKLAMUSIC/utils/decorators/play.py
+++ SHUKLAMUSIC/utils/decorators/play.py
@@ -118,7 +118,7 @@ def PlayWrapper(command):
             userbot = await get_assistant(chat_id)
             try:
                 try:
-                    get = await app.get_chat_member(chat_id, userbot.username)
+                    get = await app.get_chat_member(chat_id, userbot.id)
                 except ChatAdminRequired:
                     return await message.reply_text(_["call_1"])
                 if (
